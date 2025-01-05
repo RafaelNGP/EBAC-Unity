aba12208e4c3e30788b0bde61a1f7ca2fcecaac5
Rafael Abud Ferreira <rafaelferreira.dev3@gmail.com>
2025-01-05 01:53:39 -0300

    v1 project 2D plat sprite bones + script

--- Projetos/2DPlataform/Assets/Scripts/Prototype/Player.cs
+++ Projetos/2DPlataform/Assets/Scripts/Prototype/Player.cs
@@ -5,30 +5,30 @@ public class Player : MonoBehaviour
 {
     [Header("Movement Setup")]
     public Rigidbody2D rigidbody2;
-    public Vector2 friction = new Vector2(.1f, 0);
+    public Vector2 friction = new(.1f, 0);
+    public Vector2 playerSizeL = new(-.5f, .5f);
+    public Vector2 playerSizeR = new(.5f, .5f);
     public float speed;
     public float speedRun;
     public float forceJump = 2;
 
-    private float _currentSpeed;
     private bool _isRunning;
+    private bool _isWalkingRight;
 
     [Header("Animation Setup")]
-    public float startJumpY = 1.5f;
-    public float startJumpX = .7f;
-    public float endJumpY = .7f;
-    public float endJumpX = 1.5f;
+    public float startJumpY;
+    public float startJumpX;
+    public float endJumpY;
+    public float endJumpX;
+
+    [SerializeField] private Animator animator;
 
     private bool _isJumping;
-
     private float jumpDuration = .3f;
 
     [Header("Attack Setup")]
     [SerializeField] GameObject projectile;
-    [SerializeField] Transform firepointLeft;
-    [SerializeField] Transform firepointRight;
-    private bool _isMovingLeft;
-    private bool _isMovingRight;
+    [SerializeField] Transform firepoint;
 
     // Update is called once per frame
     void Update()
@@ -37,7 +37,31 @@ public class Player : MonoBehaviour
         HandleJump();
         HandleAttack();
     }
+    private void HandleMovement()
+    {
+        _isRunning = Input.GetKey(KeyCode.LeftShift);
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetBool("Walking", true);
+            gameObject.transform.localScale = playerSizeL;
+            firepoint.rotation = Quaternion.Euler(0, 0, -180);
+            rigidbody2.velocity = new Vector2(_isRunning ? -speedRun : -speed, rigidbody2.velocity.y);
+            _isWalkingRight = false;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("Walking", true);
+            gameObject.transform.localScale = playerSizeR;
+            firepoint.rotation = Quaternion.Euler(0, 0, 0);
+            rigidbody2.velocity = new Vector2(_isRunning ? speedRun : speed, rigidbody2.velocity.y);
+            _isWalkingRight = true;
+        }
+        else animator.SetBool("Walking", false);
+
+        if (rigidbody2.velocity.x > 0) rigidbody2.velocity += friction;
+        else if (rigidbody2.velocity.x < 0) rigidbody2.velocity -= friction;
+    }
     private void HandleJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -46,48 +70,20 @@ public class Player : MonoBehaviour
             HandleAnimation();
         }
     }
+
     private void HandleAnimation()
     {
-        rigidbody2.transform.localScale = Vector2.one;
         DOTween.Kill(rigidbody2.transform);
         rigidbody2.transform.DOScaleY(startJumpY, jumpDuration).SetLoops(2, LoopType.Yoyo);
-        rigidbody2.transform.DOScaleX(startJumpX, jumpDuration).SetLoops(2, LoopType.Yoyo);
+        rigidbody2.transform.DOScaleX(_isWalkingRight ? startJumpX : -startJumpX, jumpDuration).SetLoops(2, LoopType.Yoyo);
         _isJumping = true;
-    }
-    private void HandleMovement()
-    {
-        _isRunning = Input.GetKey(KeyCode.LeftShift);
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rigidbody2.velocity = new Vector2(_isRunning ? -speedRun : -speed, rigidbody2.velocity.y);
-            _isMovingLeft = true;
-            _isMovingRight = false;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rigidbody2.velocity = new Vector2(_isRunning ? speedRun : speed, rigidbody2.velocity.y);
-            _isMovingLeft = false;
-            _isMovingRight = true;
-        }
-
-        if (rigidbody2.velocity.x > 0)
-        {
-            rigidbody2.velocity += friction;
-        }
-        else if (rigidbody2.velocity.x < 0)
-        {
-            rigidbody2.velocity -= friction;
-        }
     }
 
     private void HandleAttack()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            Instantiate(projectile, 
-                _isMovingLeft ? firepointLeft.position : firepointRight.position, 
-                _isMovingLeft ? firepointLeft.rotation : firepointRight.rotation);
+            Instantiate(projectile, firepoint.position, firepoint.rotation);
         }
     }
 
@@ -99,7 +95,7 @@ public class Player : MonoBehaviour
             {
                 DOTween.Kill(rigidbody2.transform);
                 rigidbody2.transform.DOScaleY(endJumpY, jumpDuration).SetLoops(2, LoopType.Yoyo);
-                rigidbody2.transform.DOScaleX(endJumpX, jumpDuration).SetLoops(2, LoopType.Yoyo);
+                rigidbody2.transform.DOScaleX(_isWalkingRight ? endJumpX : -endJumpX, jumpDuration).SetLoops(2, LoopType.Yoyo);
                 _isJumping = false;
             }
         }
