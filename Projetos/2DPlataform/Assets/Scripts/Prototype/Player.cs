5aa4c84106d86b15491038e692145b1d18fa6c77
Rafael Abud Ferreira <rafaelferreira.dev3@gmail.com>
2025-04-11 21:42:14 -0300

    otimizações e mais animações

--- Projetos/2DPlataform/Assets/Scripts/Prototype/Player.cs
+++ Projetos/2DPlataform/Assets/Scripts/Prototype/Player.cs
@@ -1,105 +1,82 @@
 using UnityEngine;
-using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
-    [Header("Movement Setup")]
-    public Rigidbody2D rigidbody2;
-    public Vector2 friction = new(.1f, 0);
-    public Vector2 playerSizeL = new(-.5f, .5f);
-    public Vector2 playerSizeR = new(.5f, .5f);
-    public float speed;
-    public float speedRun;
-    public float forceJump = 2;
+    [Header("Movimentação")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 12f;
+    private float horizontalInput;
 
-    private bool _isRunning;
-    private bool _isWalkingRight;
+    [Header("Checagem de Solo")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
 
-    [Header("Animation Setup")]
-    public float startJumpY;
-    public float startJumpX;
-    public float endJumpY;
-    public float endJumpX;
+    [Header("Componentes")]
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector3 originalScale;
 
-    [SerializeField] private Animator animator;
-
-    private bool _isJumping;
-    private float jumpDuration = .3f;
-
-    [Header("Attack Setup")]
-    [SerializeField] GameObject projectile;
-    [SerializeField] Transform firepoint;
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        HandleMovement();
-        HandleJump();
-        HandleAttack();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        originalScale = transform.localScale;
     }
-    private void HandleMovement()
-    {
-        _isRunning = Input.GetKey(KeyCode.LeftShift);
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+    private void Update()
+    {
+        // Entrada de movimento
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        // Checagem de solo
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Lógica de pulo
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            animator.SetBool("Walking", true);
-            gameObject.transform.localScale = playerSizeL;
-            firepoint.rotation = Quaternion.Euler(0, 0, -180);
-            rigidbody2.velocity = new Vector2(_isRunning ? -speedRun : -speed, rigidbody2.velocity.y);
-            _isWalkingRight = false;
+            Jump();
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            animator.SetBool("Walking", true);
-            gameObject.transform.localScale = playerSizeR;
-            firepoint.rotation = Quaternion.Euler(0, 0, 0);
-            rigidbody2.velocity = new Vector2(_isRunning ? speedRun : speed, rigidbody2.velocity.y);
-            _isWalkingRight = true;
-        }
-        else animator.SetBool("Walking", false);
 
-        if (rigidbody2.velocity.x > 0) rigidbody2.velocity += friction;
-        else if (rigidbody2.velocity.x < 0) rigidbody2.velocity -= friction;
-    }
-    private void HandleJump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody2.velocity = Vector2.up * forceJump;
-            HandleAnimation();
-        }
+        // Atualiza parâmetros de animação
+        UpdateAnimations();
     }
 
-    private void HandleAnimation()
+    private void FixedUpdate()
     {
-        DOTween.Kill(rigidbody2.transform);
-        rigidbody2.transform.DOScaleY(startJumpY, jumpDuration).SetLoops(2, LoopType.Yoyo);
-        rigidbody2.transform.DOScaleX(_isWalkingRight ? startJumpX : -startJumpX, jumpDuration).SetLoops(2, LoopType.Yoyo);
-        _isJumping = true;
+        Move();
     }
 
-    private void HandleAttack()
+    private void Move()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        if (horizontalInput < 0)
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        else if (horizontalInput > 0)
+            transform.localScale = originalScale;
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void UpdateAnimations()
+    {
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+        animator.SetFloat("YVelocity", rb.velocity.y);
+    }
+
+    // Gizmo para visualização do GroundCheck
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
         {
-            animator.SetTrigger("Attacking");
-            Instantiate(projectile, firepoint.position, firepoint.rotation);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
     }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.transform.CompareTag("Floor"))
-        {
-            if (_isJumping)
-            {
-                DOTween.Kill(rigidbody2.transform);
-                rigidbody2.transform.DOScaleY(endJumpY, jumpDuration).SetLoops(2, LoopType.Yoyo);
-                rigidbody2.transform.DOScaleX(_isWalkingRight ? endJumpX : -endJumpX, jumpDuration).SetLoops(2, LoopType.Yoyo);
-                _isJumping = false;
-            }
-        }
-    }
-
 }
